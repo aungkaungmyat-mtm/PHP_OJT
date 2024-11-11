296c0c9da64b981f5369b56b1ef7bb437cba016d
aungkaungmyat-mtm <scm.aungkaungmyat@gmail.com>
2024-11-11 12:10:18 +0630

    Update pr_controller.php

--- app/Http/Controllers/pr_controller.php
+++ app/Http/Controllers/pr_controller.php
@@ -55,7 +55,7 @@ class UserController extends Controller {
     public function deleteUser($id) {
         $user = User:findOrFail($id);
         if ($user) {
-            user->delete();
+            $user->delete();
         }
         return back()->with('message', "User deleted");
     }
